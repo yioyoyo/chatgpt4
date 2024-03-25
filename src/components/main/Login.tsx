e2ae870c76253d984be83770292c774e072305d9
1042782229 <1042782229@qq.com>
2024-03-25 04:05:38 +0000

    1

--- src/components/main/Login.tsx
+++ src/components/main/Login.tsx
@@ -95,6 +95,7 @@ export default (props: Props) => {
       <button onClick={login} class="w-1/3 h-12 mt-2 px-4 py-2 bg-slate bg-op-15 hover:bg-op-20 rounded-sm">
         开启4.0
       </button>
+      <p class="mt-2">⚠️ 使用本平台所提供的任何服务请务必遵循中华人民共和国法律政策，请勿用于非法民事活动，本平台仅供学习交流使用</p>
     </div>
   )
 }
